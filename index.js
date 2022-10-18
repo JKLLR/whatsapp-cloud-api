9cd7751cf729100874ed089e8e11dcca50baee40
JKLLR <jeffhuria@gmail.com>
2022-10-18 17:06:25 +0300

    fix bug

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.listen(process.env.PORT, () => {
 
 //to verify the callback url from dashboard side(cloud api side)
 app.get("/webhook", (req, res) => {
-    let mode = req.query("hub.mode");
-    let challenge = req.query("hub.challenge");
-    let token = req.query("hub.verify_token");
+    let mode = req.query["hub.mode"];
+    let challenge = req.query["hub.challenge"];
+    let token = req.query["hub.verify_token"];
 
 
     if (mode && token) {
@@ -37,6 +37,7 @@ app.post("/webhook", (req, res) => {
     console.log(JSON.stringify(body_param, null, 2));
 
     if (body_param.object) {
+        // console.log("inside body param");
         if (body_param.entry &&
             body_param.entry[0].changes &&
             body_param.entry[0].changes[0].value.messages &&
@@ -45,6 +46,10 @@ app.post("/webhook", (req, res) => {
             let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+            // console.log("phone number " + phon_no_id);
+            // console.log("from " + from);
+            // console.log("boady param " + msg_body);
 
             // Send a POST request
             axios({
@@ -71,6 +76,6 @@ app.post("/webhook", (req, res) => {
     }
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).send("hello this is running");
 });
